5adb33ee1cd32c1d06a6744e0c53c9ed1c97d986
Georgios Valotasios <valotas@gmail.com>
2014-09-21 21:06:29 +0200

    Set the dotfiles dir with pwd

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=`pwd`                   # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="vimrc vim fonts bashrc"    # list of files/folders to symlink in homedir
 
