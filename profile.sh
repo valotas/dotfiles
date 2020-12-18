56fe1dfbc577101cd35f19633216b12bdeede93a
Georgios Valotasios <valotas@gmail.com>
2020-12-18 15:34:58 +0100

    fix: add generic profile.sh
    it can be sourced in both bashrc and zsh

---
+++ profile.sh
@@ -0,0 +1,5 @@
+export FOOTBALL_DATA_ORG_TOKEN=5ecd3132cf3b4299b70e7229192a49bd
+export FOOTBALL_DATA_ORG_USER=valotas@gmail.com
+
+export SDKMAN_DIR="${HOME}/.sdkman"
+[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
