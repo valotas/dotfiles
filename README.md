050c74dde73a7b73621494f46569ab0cf7bc2777
Georgios Valotasios <valotas@gmail.com>
2022-08-12 22:46:11 +0200

    feat: make use of stow for link management

--- README.md
+++ README.md
@@ -3,11 +3,14 @@ Just a collection of dotfiles that can be used in any pc of mine
 
 ## Installation
 
+The following
+
 ```sh
 git clone git@github.com:valotas/dotfiles.git
-cd dotfiles
-./setup.sh
+find  -maxdepth 1 -path './[^.]*' -type d -print | sed "s|^\./||" | xargs -L 1 stow
 ```
+
+should install every config under the existing directories
 
 ### Vim
 There are some more things that should be taken care of for vim.
@@ -18,5 +21,10 @@ The `taglist` bundle needs the `exuberant-ctags` package. So just do a `sudo apt
 ### Airline
 `vim-airline` need a patched version of fonts. Although I set that, I have to make sure that my terminal will be using the same fonts if I want to use `vim` within it. So go to `Edit > Profiles` select your profile and click edit. There change teh font at the first tab to `Droid Sans Mono for Powerline` which should be installed.
 
-### jshint2.vim
-`jshint2.vim` uses nodejs with the `jshint` to do the linting and that means that you must have them both installed. For the later just do a `sudo npm install -g jshint`
+## Uninstalling 
+
+In order to uninstall a directory:
+
+```sh
+stow -D [directory]
+```
--- setup.sh
+++ setup.sh
@@ -1,41 +1,13 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
-
-dir=`pwd`                   # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim fonts tmux.conf"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-echo "Updating submodules"
-git submodule update --init
-git submodule foreach git pull origin master
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+echo "stow configs"
+find  -maxdepth 1 -path './[^.]*' -type d -print | sed "s|^\./||" | xargs -L 1 stow
 
 #update the font cache
 echo "Updating font cache"
 fc-cache -vf ~/.fonts
-echo "...done"
+
+#echo "Updating submodules"
+#git submodule update --init
+#git submodule foreach git pull origin master
+#echo "...done"
