c7cdb44ec752fb810d0a4bda361507bcee08b4b4
Georgios Valotasios <valotas@gmail.com>
2022-08-13 08:57:55 +0200

    feat: add .profile

--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
 # this should be sourced in .profile and/or .zenv:
 # [[ -s "$HOME/.dotfiles/env.sh" ]] && . "$HOME/.dotfiles/env.sh"
 
+export DOTFILES_PATH="$HOME/.dotfiles"
+
 export SYSTEMD_EDITOR="vim"
 
 # Preferred editor for local and remote sessions
@@ -21,7 +23,7 @@ export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 # for debugging
-export VALOTAS_VERSION="$HOME/dotfiles/env.sh"
+export VALOTAS_VERSION="$DOTFILES_PATH/env.sh"
 
 # flyctl
 if [[ -f "$HOME/.fly/bin/flyctl" ]]; then
