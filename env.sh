be45c6b02ccbb97db53f2e7720ad4f9a53106f39
Georgios Valotasios <valotas@gmail.com>
2022-08-13 08:48:45 +0200

    Add EDITOR and FLYCTL variables

--- env.sh
+++ env.sh
@@ -1,5 +1,16 @@
 # this should be sourced in .profile and/or .zenv:
-# [[ -s "$HOME/dotfiles/env.sh" ]] && . "$HOME/dotfiles/env.sh"
+# [[ -s "$HOME/.dotfiles/env.sh" ]] && . "$HOME/.dotfiles/env.sh"
+
+export SYSTEMD_EDITOR="vim"
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then # SSH mode
+  export EDITOR='vim'
+else # Local terminal mode
+  export EDITOR='code -w'
+fi
+
+export VISUAL="$EDITOR"
 
 # football-data.org stuff
 export FOOTBALL_DATA_ORG_TOKEN="5ecd3132cf3b4299b70e7229192a49bd"
@@ -10,4 +21,10 @@ export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 # for debugging
-export VALOTAS_VERSION="dotfiles/env.sh"
+export VALOTAS_VERSION="$HOME/dotfiles/env.sh"
+
+# flyctl
+if [[ -f "$HOME/.fly/bin/flyctl" ]]; then
+  export FLYCTL_INSTALL="$HOME/.fly"
+  export -U PATH=$FLYCTL_INSTALL/bin${PATH:+:$PATH}
+fi
