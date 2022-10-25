ee9e41ce6b1483cdda44e657aa2a166c0effa53c
Georgios Valotasios <valotas@gmail.com>
2022-10-25 17:30:43 +0200

    fix: load NVM if not loaded

--- sh_setup.sh
+++ sh_setup.sh
@@ -3,3 +3,6 @@ export _VALOTAS_ENV_COUNTER="${_VALOTAS_ENV_COUNTER}[s]"
 # sdkman
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+# nvm
+[[ -z "$NVM_BIN" ]] && [[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"  # This loads NVM
