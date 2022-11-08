3ff998d58fec239353feba9cbe731dd061288e68
Georgios Valotasios <valotas@gmail.com>
2022-11-08 19:22:11 +0100

    fix: load nvm as soon as nvm.sh exists

--- sh_setup.sh
+++ sh_setup.sh
@@ -5,7 +5,7 @@ export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 # nvm
-[[ -z "$NVM_BIN" ]] && [[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"  # This loads NVM
+[[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"  # This loads NVM
 
 # starship
 if [[ $(command -v starship) ]]; then
