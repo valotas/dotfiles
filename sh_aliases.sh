01abd8b5cf2f9b39956186ec4150b32e1655aca7
Georgios Valotasios <valotas@gmail.com>
2025-03-28 12:37:32 +0100

    fix: add `vi`/`vim` = `nvim`

--- sh_aliases.sh
+++ sh_aliases.sh
@@ -6,3 +6,9 @@ alias la="ls -al"
 if [[ -f "$HOME/.config/local_aliases.sh" ]]; then
   source "$HOME/.config/local_aliases.sh"
 fi
+
+# Check if nvim is installed, and if so, alias vim/vi to nvim
+if command -v nvim >/dev/null 2>&1; then
+  alias vim='nvim'
+  alias vi='nvim'
+fi
