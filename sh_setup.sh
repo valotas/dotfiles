0567dcbe2a1e2443265e7d528426ccf1703cfe76
Georgios Valotasios <valotas@gmail.com>
2025-05-04 13:45:32 +0200

    fix(profile): rework profile loading
    - do not export `SDKMAN_DIR` if it is not installed
    - do not compote the active shell everytime we need to use it

--- sh_setup.sh
+++ sh_setup.sh
@@ -1,22 +1,26 @@
 export _VALOTAS_ENV_COUNTER="${_VALOTAS_ENV_COUNTER}[s]"
 
 # sdkman
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+sdkman="$HOME/.sdkman"
+if [[ -s "$sdkman/bin/sdkman-init.sh" ]]; then
+  source "$sdkman/bin/sdkman-init.sh"
+  export SDKMAN_DIR="$sdkman"
+fi
 
 # nvm
 [[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"  # This loads NVM
 
+# check the current shell (will be the full path of the shell)
+shell="$(ps -p $$ -o comm=)"
+
 # vfox
 if [[ $(command -v vfox) ]]; then 
-  shell="$(ps -p $$ -o comm=)"
   [[ $shell == *"bash" ]] && eval "$(vfox activate bash)"
   [[ $shell == *"zsh" ]] && eval "$(vfox activate zsh)"
 fi
 
 # starship
 if [[ $(command -v starship) ]]; then
-  shell="$(ps -p $$ -o comm=)"
   [[ $shell == *"bash" ]] && eval "$(starship init bash)"
   [[ $shell == *"zsh" ]] && eval "$(starship init zsh)"
 fi
