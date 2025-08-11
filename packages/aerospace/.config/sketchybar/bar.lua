c40282ca664791f9e35bd2ef136b8c8e027742d4
Georgios Valotasios <valotas@gmail.com>
2025-08-11 10:16:13 +0200

    feat(aerospace): init 2 sketchybar instances

--- packages/aerospace/.config/sketchybar/bar.lua
+++ packages/aerospace/.config/sketchybar/bar.lua
@@ -1,9 +1,46 @@
 local colors = require("colors")
+local logging = require("helpers.logging")
 
--- Equivalent to the --bar domain
-sbar.bar({
-  height = 40,
-  color = colors.bar.bg,
-  padding_right = 5,
-  padding_left = 5,
-})
+local bar = {}
+
+local function get_displays()
+  local displays = sbar.query("displays")
+
+  local main_display = 0
+  local alternate_display = {}
+
+  for _, display in ipairs(displays) do
+    local displayId = display["arrangement-id"]
+    if display.UUID == "37D8832A-2D66-02CA-B9F7-8F30A301B230" then
+      main_display = displayId
+    else 
+      table.insert(alternate_display, displayId)
+    end
+  end
+  return {
+    -- the build in display
+    main = main_display,
+
+    -- the external displays
+    alternate = alternate_display,
+  }
+end
+
+bar.create_bar_config = function(bar_name)
+  local main_bar = bar_name == "sketchybar_main"
+  local displays = get_displays()
+  local display = main_bar and displays.main > 0 and displays.main or table.concat(displays.alternate, ",")
+  local height = main_bar and 40 or 30
+
+  logging.log("Creating bar config for " .. bar_name .. " on display " .. display .. " with height " .. height)
+  return {
+    height = height,
+    hidden = bar_name == "sketchybar_main" and displays.main == 0 or bar_name == "sketchybar_alternate" and #displays.alternate == 0,
+    color = colors.bar.bg,
+    padding_right = bar_name == "sketchybar_main" and 5 or 15,
+    padding_left = bar_name == "sketchybar_main" and 5 or 15,
+    display = display,
+  }
+end
+
+return bar
--- packages/aerospace/.config/sketchybar/init.lua
+++ packages/aerospace/.config/sketchybar/init.lua
@@ -1,15 +1,24 @@
 -- Require the sketchybar module
 sbar = require("sketchybar")
+local bar = require("bar")
+local logging = require("helpers.logging")
 
 -- Set the bar name, if you are using another bar instance than sketchybar
-sbar.set_bar_name(os.getenv("BAR_NAME"))
+local bar_name = os.getenv("BAR_NAME")
+logging.log("Setting bar name to " .. bar_name)
+sbar.set_bar_name(bar_name)
 
 -- Bundle the entire initial configuration into a single message to sketchybar
 sbar.begin_config()
-require("bar")
+sbar.bar(bar.create_bar_config(bar_name))
 require("default")
 require("items")
 sbar.end_config()
+
+sbar.add("event", "display_change", "hs.screen.watcher")
+sbar.subscribe({"display_change"}, function()
+  logging.log("Display changed")
+end)
 
 -- Run the event loop of the sketchybar module (without this there will be no
 -- callback functions executed in the lua module)
