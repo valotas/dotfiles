06be643320f9041d5b7f34d751abda9427a2a05e
Georgios Valotasios <valotas@gmail.com>
2026-07-22 16:12:49 +0200

    fix: add mise

--- sh_setup.sh
+++ sh_setup.sh
@@ -1,20 +1,24 @@
 export _VALOTAS_ENV_COUNTER="${_VALOTAS_ENV_COUNTER}[s]"
 
 # sdkman
-sdkman="$HOME/.sdkman"
-if [[ -s "$sdkman/bin/sdkman-init.sh" ]]; then
-  source "$sdkman/bin/sdkman-init.sh"
-  export SDKMAN_DIR="$sdkman"
-fi
-
-# nvm
-[[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"  # This loads NVM
+#sdkman="$HOME/.sdkman"
+#if [[ -s "$sdkman/bin/sdkman-init.sh" ]]; then
+#  source "$sdkman/bin/sdkman-init.sh"
+#  export SDKMAN_DIR="$sdkman"
+#fi
+#
+## nvm
+#[[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"  # This loads NVM
 
 # check the current shell (will be the full path of the shell)
 shell="$(ps -p $$ -o comm=)"
 
+# mise
+if [[ $(command -v mise) ]]; then
+  export PATH="/opt/homebrew/bin:$PATH"
+  export PATH="$HOME/.local/share/mise/shims:$PATH"
 # vfox
-if [[ $(command -v vfox) ]]; then 
+elif [[ $(command -v vfox) ]]; then
   [[ $shell == *"bash" ]] && eval "$(vfox activate bash)"
   [[ $shell == *"zsh" ]] && eval "$(vfox activate zsh)"
 fi
@@ -25,7 +29,7 @@ if [[ $(command -v starship) ]]; then
   if [[ "$(hostname -s)" == "m4air" ]]; then
     export STARSHIP_MAIN_HOST=1
   fi
-  
+
   [[ $shell == *"bash" ]] && eval "$(starship init bash)"
   [[ $shell == *"zsh" ]] && eval "$(starship init zsh)"
 fi
